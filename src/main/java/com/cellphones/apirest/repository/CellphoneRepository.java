4a860f563a881722754479f89f3bcf8c15237adb
GuilhermeMuchon <guilherme.muchon@gmail.com>
2020-07-16 22:18:38 -0300

    get and post method implementation

--- src/main/java/com/cellphones/apirest/repository/CellphoneRepository.java
+++ src/main/java/com/cellphones/apirest/repository/CellphoneRepository.java
@@ -1,9 +1,10 @@
-package com.produtos.apirest.repository;
+package com.cellphones.apirest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cellphones.apirest.models.Cellphone;
 
 public interface CellphoneRepository extends JpaRepository<Cellphone, Long> {
-
+	Cellphone findById(long id);
 }
---
+++ src/main/java/com/cellphones/apirest/resources/CellphoneResource.java
@@ -0,0 +1,36 @@
+package com.cellphones.apirest.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cellphones.apirest.models.Cellphone;
+import com.cellphones.apirest.repository.CellphoneRepository;
+
+@RestController
+@RequestMapping(value = "/api")
+public class CellphoneResource {
+	@Autowired
+	CellphoneRepository cellphoneRepository;
+
+	@GetMapping("/cellphones")
+	public List<Cellphone> cellphoneList() {
+		return cellphoneRepository.findAll();
+	}
+
+	@GetMapping("/cellphone/{id}")
+	public Cellphone cellphoneListSpecific(@PathVariable(value="id") long id) {
+		return cellphoneRepository.findById(id);
+	}
+	
+	@PostMapping("/cellphone")
+	public Cellphone insertCellphone(@RequestBody Cellphone cellphone) {
+		return cellphoneRepository.save(cellphone);
+	}
+}
